93c6b0033e72d5c94bb1818267fecc6b11bd3e3c
Len <a78965@gmail.com>
2012-08-04 20:47:36 +0800

    LICENSE independent.

--- notify.py
+++ notify.py
@@ -1,31 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-'''
-# fb-notify
-
-Copyright (C) 2012 Len @ http://len.hack-stuff.com
-# All rights reversed.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom
-the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-'''
-
 import urllib2
 import urllib
 import json
@@ -37,9 +12,69 @@ import sys
 import pynotify
 
 from lib import fbconsole
-
+from optparse import OptionParser
 
 tmp = os.path.dirname( os.path.abspath(__file__) ) + '/tmp/'
+
+guide = "usage: %prog [options]"
+parser = OptionParser(usage=guide)
+
+parser.add_option('-v','--verbose',action='store_false',help='verbose mode',dest='verbose')
+parser.add_option('-m','--mark',action='store_false',help='auto mark notifications as read',dest='mark')
+
+(options,args) = parser.parse_args()
+
+#pynotify.Notification method
+def notify(title,msg,photo):
+	n = pynotify.Notification (title,msg,photo) #"notification-message-im"
+	n.show ()
+
+class notf:
+    def __init__(self):
+        pass
+    def notfy(self):
+        try:
+            #fbconsole method
+            fbconsole.AUTH_SCOPE = ['manage_notifications']
+            fbconsole.authenticate()
+
+            time.sleep(random.randint(30,120))
+        
+            #facebook ID
+            ID = 'len.tw'
+        
+            #Graph API access_token
+
+            #print fbconsole.ACCESS_TOKEN
+            token = fbconsole.ACCESS_TOKEN
+
+            req = urllib2.Request( 'https://graph.facebook.com/'+ ID + '/notifications?access_token=' + token )
+            req.add_header( 'User-agent' , 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6' )
+            try: r = urllib2.urlopen( req )
+            except urllib2.URLError:
+               pass 
+
+            jr = json.load( r ) 
+
+            for data in jr["data"]:
+                if data == '' :
+                    continue
+                name = data["from"]["name"]  # notify from who
+                msg  = data["title"]         # notify summary
+                photoID = data["from"]["id"] # Got notify from who's ID
+
+                # Cache avatar
+                photo = 'https://graph.facebook.com/' + photoID + '/picture'
+                tmpPhoto = tmp + photoID
+                urllib.urlretrieve(photo,tmpPhoto)
+
+                notify(name,msg,tmpPhoto)
+
+        # HOLD Ctrl+c event
+        except KeyboardInterrupt:
+            fbconsole.logout()
+            print '\nLog out'
+            exit(0)
 
 if __name__ == '__main__':
     if not pynotify.init ("icon-summary-body"):
@@ -55,51 +90,5 @@ if __name__ == '__main__':
             print 'cannot mkdir tmp'
             sys.ext(1)
         print 'success'
-
-#pynotify.Notification method
-def notify(title,msg,photo):
-	n = pynotify.Notification (title,msg,photo) #"notification-message-im"
-	n.show ()
-
-#notify loop
-while(1):
-    try:
-        #fbconsole method
-        fbconsole.AUTH_SCOPE = ['manage_notifications']
-        fbconsole.authenticate()
-
-        time.sleep(random.randint(30,120))
-    
-        #facebook ID
-        ID = 'len.tw'
-    
-        #Graph API access_token
-
-        #print fbconsole.ACCESS_TOKEN
-        token = fbconsole.ACCESS_TOKEN
-
-        req = urllib2.Request( 'https://graph.facebook.com/'+ ID + '/notifications?access_token=' + token )
-        req.add_header( 'User-agent' , 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6' )
-        r = urllib2.urlopen( req )
-
-        jr = json.load( r ) 
-
-        for data in jr["data"]:
-            if data == '' :
-                continue
-            name = data["from"]["name"]  # notify from who
-            msg  = data["title"]         # notify summary
-            photoID = data["from"]["id"] # Got notify from who's ID
-
-            # Cache avatar
-            photo = 'https://graph.facebook.com/' + photoID + '/picture'
-            tmpPhoto = tmp + photoID
-            urllib.urlretrieve(photo,tmpPhoto)
-
-            notify(name,msg,tmpPhoto)
-
-    # HOLD Ctrl+c event
-    except KeyboardInterrupt:
-        fbconsole.logout()
-        print '\nLog out'
-        exit(0)
+    while(1):
+        notf().notfy()
